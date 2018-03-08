7008ddb899c7a49726d352776cf5b647689e761c
Shehan Caldera <shehancaldera@gmail.com>
2018-03-08 09:37:57 +0800

    Changed the NN to default Googlenet
    Not working. Might be becuase the default googlenet is configured for classification. I might have to change the architecture.

--- train_network.m
+++ train_network.m
@@ -1,11 +1,13 @@
-layers = [ ...
-    imageInputLayer([224 224 3])
-    convolution2dLayer(12,25)
-    reluLayer
+deepnet = googlenet;
+layersTransfer = deepnet.Layers(1:end-3);
+layers = [
+    %imageInputLayer([224 224 3])
+    layersTransfer
     fullyConnectedLayer(5)
     regressionLayer];
+
 
 options = trainingOptions('sgdm','InitialLearnRate',0.001, ...
     'MaxEpochs',2, 'MiniBatchSize',4);
 
-net = trainNetwork(train_data,layers,options)
+trained_net = trainNetwork(train_data,layers,options)
