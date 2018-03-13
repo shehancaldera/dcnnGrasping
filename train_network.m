8a881302407e28742238d80690770512341564fd
Shehan Caldera <shehancaldera@gmail.com>
2018-03-13 11:51:37 +0800

    Added the initial weights+biases
    Changed the training options and parameters.

--- train_network.m
+++ train_network.m
@@ -6,11 +6,16 @@ layers = [
     fullyConnectedLayer(5)
     regressionLayer];
 
+initConvo2DLayer = convolution2dLayer(5,64,'Stride', [2 2], 'Padding','same','BiasLearnRateFactor',2);
+initConvo2DLayer.Weights = randn([5 5 3 64]) * 0.0001;
+initConvo2DLayer.Bias = randn([1 1 64])*0.00001 + 1;
+
 nnet = [
     imageInputLayer([224 224 3])
     %conv2d-1: filterSize(5x5), num. filters/ channels/ depth of next layer, and
     %so on explained by name values
-    convolution2dLayer([5 5], 64, 'Stride',[2 2], 'Padding', 'same')
+    %convolution2dLayer([5 5], 64, 'Stride',[2 2], 'Padding', 'same')
+    initConvo2DLayer
     reluLayer
     maxPooling2dLayer([2 2])
     %=======================
@@ -54,12 +59,15 @@ nnet = [
     %output
     fullyConnectedLayer(5)
     outputLayer
+    %regressionLayer
+    
     %=======================
     ];
     
 
 
-options = trainingOptions('sgdm','InitialLearnRate',0.001, ...
-    'MaxEpochs',2, 'MiniBatchSize',4);
+options = trainingOptions('sgdm','InitialLearnRate',0.0001, ...
+    'MaxEpochs',20, 'MiniBatchSize',16);
+
 
 trained_net = trainNetwork(train_data,nnet,options)
